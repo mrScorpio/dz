b8941838bd3803595d92818ee7570850dd196b42
Valerii <valscv@yandex.ru>
2025-11-01 15:30:29 +0500

    4-order-api second try

--- 4-order-api/model.go
+++ 4-order-api/model.go
@@ -1,15 +1,16 @@
 package orderapi
 
 import (
+	"github.com/lib/pq"
 	"gorm.io/gorm"
 )
 
 type Product struct {
 	gorm.Model
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	PriceRub    int    `json:"prub"`
-	Images      string `json:"images" gorm:"default:no_image"`
+	Name        string         `json:"name"`
+	Description string         `json:"description"`
+	PriceRub    int            `json:"prub"`
+	Images      pq.StringArray `json:"images" gorm:"type:varchar(256)[]"`
 }
 
 func NewProduct(name, descr string, prub int) *Product {
